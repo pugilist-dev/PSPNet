84b33484a375b580df52db809793b546b58bf23d
Rajiv Mandya Nagaraju <64949926+rajivnmurthy31@users.noreply.github.com>
2021-01-31 13:03:12 -0500

    Add files via upload

---
+++ PSPNet/PPMNet/eval.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 13 11:53:41 2021
+
+@author: rajiv
+"""
+
+import os
+import torch
+import torch.utils.data as data
+
+from torchvision import transforms
+from data_loader import get_segmentation_dataset
+from model.ppm_api import get_model
+from utils.metric import SegmentationMetric
+from utils.visualize import get_color_pallete
+
+from train import parse_args
+
+
+class Evaluator(object):
+    def __init__(self, args):
+        self.args = args
+        # output folder
+        self.outdir = 'test_result'
+        if not os.path.exists(self.outdir):
+            os.makedirs(self.outdir)
+        # image transform
+        input_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([.485, .456, .406], [.229, .224, .225]),
+        ])
+        # dataset and dataloader
+        val_dataset = get_segmentation_dataset(args.dataset, split='val', mode='testval',
+                                               transform=input_transform)
+        self.val_loader = data.DataLoader(dataset=val_dataset,
+                                          batch_size=1,
+                                          shuffle=False)
+        # create network
+        self.model = get_model().to(self.args.device)
+        self.model.load_state_dict(torch.load("/home/rajiv/Documents/papers/API-Semantic-Segmentation/weights/psp_citys.pth"))
+        print('Finished loading model!')
+
+        self.metric = SegmentationMetric(val_dataset.num_class)
+
+    def eval(self):
+        self.model.eval()
+        avgPixAcc = 0
+        avgmIoU = 0
+        for i, (image, label) in enumerate(self.val_loader):
+            image = image.to(self.args.device)
+            outputs = self.model(image, flag='eval')
+
+            pred = torch.argmax(outputs[0], 1)
+            pred = pred.cpu().data.numpy()
+            label = label.numpy()
+
+            self.metric.update(pred, label)
+            pixAcc, mIoU = self.metric.get()
+            avgPixAcc += pixAcc
+            avgmIoU += mIoU
+            print('Sample %d, validation pixAcc: %.3f%%, mIoU: %.3f%%' % (i + 1, pixAcc * 100, mIoU * 100))
+
+            predict = pred.squeeze(0)
+            mask = get_color_pallete(predict, self.args.dataset)
+            mask.save(os.path.join(self.outdir, 'seg_{}.png'.format(i)))
+        avgPixAcc = torch.true_divide(avgPixAcc, len(self.val_loader)) * 100
+        avgmIoU = torch.true_divide(avgmIoU, len(self.val_loader)) * 100
+        print(" The average Pixel Accuracy and mIoU:", avgPixAcc, avgmIoU)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    evaluator = Evaluator(args)
+    print('Testing model: ', args.model)
+    evaluator.eval()
